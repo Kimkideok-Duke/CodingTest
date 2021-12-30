8fac094cd956eeb5a879600be926831fe8f85a14
Kimkideok-Duke <65582257+Kimkideok-Duke@users.noreply.github.com>
2021-12-30 23:34:30 +0900

    Upload files_Programers_JAVA
    해쉬1, 정렬1번 문제만 품.
    프로그래머스 문제를 자바로 품.

---
+++ Hash_2.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        boolean answer = true;
+        return answer;
+    }
+}
---
+++ Sorting_1.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int n=0;n<commands.length;n++)
+        {
+            int[] arr = Arrays.copyOfRange(array,commands[n][0]-1,commands[n][1]);
+            Arrays.sort(arr);
+            answer[n] = arr[commands[n][2]-1];
+        }
+        return answer;
+    }
+}
